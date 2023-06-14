3351e7b09430dca12a9dee7e359b1df1e2a9f393
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2023-06-15 00:51:49 +0300

    Fixed imports

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,5 @@
 import { Navbar } from "@/components/navbar";
 import { SquircleDemoSection } from "./SquircleDemoSection";
-import { SquircleElement } from "@squircle-js/react";
 import { UsageSection } from "./UsageSection";
 import { ExamplesSection } from "./ExamplesSection";
 import { FeaturesSection } from "./FeaturesSection";
