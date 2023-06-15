64b6fe1483d56f5e1532b70269364fb1436a1af3
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2023-06-15 16:10:39 +0300

    Fixed links

--- apps/web/lib/siteconfig.ts
+++ apps/web/lib/siteconfig.ts
@@ -3,12 +3,12 @@ export const SITECONFIG = {
   title: "Squircle Button for Frontend Projects",
   description:
     "Use an iOS-style squircle in your frontend project easily. React.js and other versions available.",
-  githubLink: "https://github.com/bring-shrubbery/squircle-element",
+  githubLink: "https://github.com/bring-shrubbery/squircle-js",
   sponsorLink: "https://github.com/sponsors/bring-shrubbery",
   linkedinLink: "https://linkedin.com/antoni-silvestrovic",
   quassumLink: "https://quassum.com/?utm_source=squircle-element",
   licenseLink:
-    "https://github.com/bring-shrubbery/squircle-element/blob/master/LICENSE",
+    "https://github.com/bring-shrubbery/squircle-js/blob/master/LICENSE",
   antoniGithubLink: "https://github.com/bring-shrubbery",
 };
 
