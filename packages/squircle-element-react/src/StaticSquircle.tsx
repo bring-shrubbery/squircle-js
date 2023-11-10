84a547744272e78abbdef08e56017053a4f9a70f
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2023-11-10 19:26:34 +0200

    StaticSquircle (#11)
    * Added StaticSquircle test component
    
    * Added missing `style` prop.
    
    * Added changeset for test component
    
    * Added test component version
    
    * Exited pre mode
    
    * commit: true
    
    * docs(changeset): Static squircle test

---
+++ packages/squircle-element-react/src/StaticSquircle.tsx
@@ -0,0 +1,39 @@
+import { Slot } from "@radix-ui/react-slot";
+import { getSvgPath } from "figma-squircle";
+import { ComponentPropsWithoutRef, PropsWithChildren, useMemo } from "react";
+
+type StaticSquircleProps = {
+  asChild?: boolean;
+
+  width: number;
+  height: number;
+  cornerRadius: number;
+  cornerSmoothing: number;
+};
+
+export const StaticSquircle = ({
+  asChild,
+  width,
+  height,
+  cornerRadius,
+  cornerSmoothing,
+  style,
+  ...props
+}: PropsWithChildren<
+  StaticSquircleProps & ComponentPropsWithoutRef<"div">
+>) => {
+  const Component = asChild ? Slot : "div";
+
+  const path = useMemo(() => {
+    return getSvgPath({
+      width,
+      height,
+      cornerRadius,
+      cornerSmoothing,
+    });
+  }, [width, height, cornerRadius, cornerSmoothing]);
+
+  return (
+    <Component style={{ clipPath: `path('${path}')`, ...style }} {...props} />
+  );
+};
