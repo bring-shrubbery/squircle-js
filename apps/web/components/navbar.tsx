ef560c3f8386a9c61e2e9f6299aa4aa65fc5cb37
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-02-03 21:46:27 +0200

    Update navbar

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import { IoLogoGithub } from "react-icons/io5";
 import { SITECONFIG } from "@/lib/siteconfig";
+import { useAtom } from "jotai";
+import { LANGUAGE_SELECTOR_ATOM } from "@/lib/atoms";
 
 export const Navbar = () => {
+  const [language] = useAtom<"react" | "svelte">(LANGUAGE_SELECTOR_ATOM);
+
   return (
     <nav className="border-gray-200 bg-white">
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-between p-6">
-        <div className="line-clamp-1 font-bold font-mono text-2xl">
-          @squircle.js
+        <div className="line-clamp-1 font-mono font-semibold text-2xl">
+          {`@squircle-js/${language}`}
         </div>
 
         <div className="w-auto">
