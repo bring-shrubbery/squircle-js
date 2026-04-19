81885357bc1a10fe1bd8de0305d3ac04d24503f7
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-04-19 13:51:49 +0300

    feat(vue): add StaticSquircle component

---
+++ packages/vue/src/StaticSquircle.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  useStaticSquircle,
+  type UseStaticSquircleOptions,
+} from "./useStaticSquircle";
+
+defineOptions({ inheritAttrs: true });
+
+const props = withDefaults(defineProps<UseStaticSquircleOptions>(), {
+  cornerSmoothing: 0.6,
+});
+
+const el = ref<HTMLDivElement | null>(null);
+
+useStaticSquircle(el, () => ({
+  width: props.width,
+  height: props.height,
+  cornerRadius: props.cornerRadius,
+  cornerSmoothing: props.cornerSmoothing,
+}));
+</script>
+
+<template>
+  <div ref="el">
+    <slot />
+  </div>
+</template>
