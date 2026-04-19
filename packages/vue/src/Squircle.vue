31293f0d4cb2aaadee3aaa90f21d7fbd3be09b3d
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-04-19 13:51:42 +0300

    feat(vue): add Squircle component

---
+++ packages/vue/src/Squircle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useSquircle, type UseSquircleOptions } from "./useSquircle";
+
+defineOptions({ inheritAttrs: true });
+
+const props = withDefaults(defineProps<UseSquircleOptions>(), {
+  cornerSmoothing: 0.6,
+});
+
+const el = ref<HTMLDivElement | null>(null);
+
+useSquircle(el, () => ({
+  cornerRadius: props.cornerRadius,
+  cornerSmoothing: props.cornerSmoothing,
+  width: props.width,
+  height: props.height,
+  defaultWidth: props.defaultWidth,
+  defaultHeight: props.defaultHeight,
+}));
+</script>
+
+<template>
+  <div ref="el">
+    <slot />
+  </div>
+</template>
